cebc59bd1d92c383526a091235ae050aa3a42618
Wesley Rocha <wesley.rocha@contractor.rhimagnesita.com>
2022-09-19 19:02:49 -0300

    feito exercicio 1006

---
+++ bee/Bee1006.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class bee1007
+{
+    public bee1007()
+    {
+        double A, B, C, media;
+
+        A = int.Parse(Console.ReadLine());
+        B = int.Parse(Console.ReadLine());
+        C = int.Parse(Console.ReadLine());
+        D = int.Parse(Console.ReadLine());
+
+        diferenca = A * B - C * D;
+
+        
+
+        Console.WriteLine("MEDIA " + media).ToString("F2", CultureInfo.InvariantCulture));
+
+    }
+}
---
+++ bee/Bee1021.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Globalization;
+
+public class Bee1021
+{
+    public bee1021()
+    {
+
+    }
+}
