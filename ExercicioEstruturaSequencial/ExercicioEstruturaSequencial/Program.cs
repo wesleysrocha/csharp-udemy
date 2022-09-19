283902a127a75e16d1ab42eba8b1452b411bac46
Wesley Rocha <wesley.rocha@contractor.rhimagnesita.com>
2022-09-19 09:46:04 -0300

    Fix de spaces

--- ExercicioEstruturaSequencial/ExercicioEstruturaSequencial/Program.cs
+++ ExercicioEstruturaSequencial/ExercicioEstruturaSequencial/Program.cs
@@ -13,8 +13,7 @@ namespace ExercicioEstruturaSequencial
             int n = int.Parse(Console.ReadLine());
             for (int i = 1; i <= n; i++)
             {
-                Console.WriteLine();
-                Console.WriteLine($"Aluguel #{i}:");
+                Console.WriteLine($"\nAluguel #{i}:");
                 Console.Write("Nome: ");
                 string nome = Console.ReadLine();
                 Console.Write("Email: ");
@@ -23,7 +22,6 @@ namespace ExercicioEstruturaSequencial
                 int quarto = int.Parse(Console.ReadLine());
                 vect[quarto] = new Estudante(nome, email);
             }
-            Console.WriteLine();
             Console.WriteLine("Quartos ocupados:");
             for (int i = 0; i < 10; i++)
             {
